92c909e25c967ab78d591089fd7e7f5ad2638ae7
Said Akhrarov <saidakhrarov21@gmail.com>
2025-04-15 18:51:24 +0500

    code is ready

--- codec/Program.cs
+++ codec/Program.cs
@@ -6,3 +6,54 @@
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → []*/
+
+
+void Main()
+
+{
+    string[] array = AskWords("Введите элементы массива через пробел: ");
+    CreateArray(array);
+    ArrayPrint(CreateArray(array));
+}
+
+string[] CreateArray(string[] array)
+{
+    int count = 0;
+    foreach (string word in array)
+    {
+        if (word.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] NewArray = new string[count];
+    int index = 0;
+
+    foreach (string word in array)
+    {
+        if (word.Length <= 3)
+        {
+            NewArray[index] = word;
+            index++;
+        }
+    }
+    return NewArray;
+}
+
+    string[] AskWords(string msg)
+    {
+        System.Console.WriteLine(msg);
+        string[] tempArray = Console.ReadLine()!.Split(' ');
+        return tempArray;
+    }
+
+    void ArrayPrint(string[] array)
+    {
+        foreach (string word in array)
+        {
+            System.Console.Write(word + ", ");
+        }
+    }
+
+    Main();
